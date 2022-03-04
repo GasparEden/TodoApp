5a255dd518bb8e84f5513cc94acc4b5c31a34050
Gaspar <gmusitelli@edenschool.fr>
2022-03-04 14:08:19 +0100

    Switch sur ts

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
-const todos = [
+var todos = [
     "Apprendre Laravel",
     "Faire le menage",
 ];
-
 function render() {
-    document.getElementById("todos").innerHTML = todos.map((v) => {
-        return `<p class="w-full mb-2">${v}</p>`;
+    document.getElementById("todos").innerHTML = todos.map(function (v) {
+        return "<p class=\"w-full mb-2\">".concat(v, "</p>");
     }).join('');
 }
-
 render();
---
+++ js/app.ts
@@ -0,0 +1,12 @@
+const todos: Array<string> = [
+    "Apprendre Laravel",
+    "Faire le menage",
+];
+
+function render() {
+    document.getElementById("todos").innerHTML = todos.map((v) => {
+        return `<p class="w-full mb-2">${v}</p>`;
+    }).join('');
+}
+
+render();
